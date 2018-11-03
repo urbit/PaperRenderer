719b5040cd919060133f97bc811349cca5c8a971
Gavin Atkinson <gvnatknsn@protonmail.com>
2018-11-03 14:34:21 -0700

    layout improvements, special renderers for ethereum addresses and patq

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import sampleWallet from './lib/sampleWallet';
+import wallet1 from './lib/wallet1.json';
+import wallet2 from './lib/wallet2.json';
+import wallet3 from './lib/wallet3.json';
+import wallet4 from './lib/wallet4.json';
+
 import PaperCollateralRenderer from './PaperCollateralRenderer';
 import LayoutGenerator from './LayoutGenerator';
 
 class App extends Component {
-
   render() {
     return (
       <div>
         <PaperCollateralRenderer
-          wallet={sampleWallet}
+          wallet={wallet4.value}
           className={''}
-          callback={data => console.log('upperscope: ', data)}
+          callback={data => console.log('App Scope: ', data)}
           mode={'CONVENTIONAL'}
         />
         {
-          // <LayoutGenerator />
+          <LayoutGenerator />
         }
       </div>
     )
--- src/LayoutGenerator.js
+++ src/LayoutGenerator.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOMServer from 'react-dom/server';
-import { map, reduce } from 'lodash';
+import { map, reduce, filter } from 'lodash';
 import figma from './lib/figma';
-
 
 const flatPack = (lo) => {
   const extracted = reduce(lo.children, (acc, child) => {
@@ -13,6 +12,10 @@ const flatPack = (lo) => {
       // if no special items are found, tranverse down into group
       return [...acc, ...flatPack(child)]
     } else {
+      if (child.name.split(':')[0] === '>patq') return [...acc, {...child, type: 'PATQ'}];
+      if (child.name.split(':')[0] === '>addr_long') return [...acc, {...child, type: 'ADDR_LONG'}];
+      if (child.name.split(':')[0] === '>addr_compact') return [...acc, {...child, type: 'ADDR_COMPACT'}];
+
       if (child.type === 'TEXT') return [...acc, {...child, type: 'TEXT'}];
       console.warn('Reminder: There are more children on board that will not be included in flatpack.')
       return acc
@@ -34,9 +37,10 @@ class LayoutGenerator extends Component {
   componentDidMount = () => {
     figma.pull('a4u6jBsdTgiXcrDGW61q5ngY', res => {
 
-      const _layouts = res.document.children[0].children;
+      const KEY = 'Layouts-rc-0.1'
+      const page = filter(res.document.children, page => page.name === KEY)[0]
 
-      const layouts = reduce(_layouts, (acc, lo) => {
+      const layouts = reduce(page.children, (acc, lo) => {
         return {
           ...acc,
           [lo.name]: {
@@ -78,13 +82,52 @@ class LayoutGenerator extends Component {
                 };
               };
 
+              if (child.type === 'PATQ') {
+                return {
+                  type: 'PATQ',
+                  fontPostScriptName: child.style.fontPostScriptName,
+                  fontSize: child.style.fontSize,
+                  text: child.characters.split(':')[1],
+                  maxWidth: child.absoluteBoundingBox.width,
+                  lineHeightPx: child.style.lineHeightPx,
+                  x: child.absoluteBoundingBox.x - lo.absoluteBoundingBox.x,
+                  y: child.absoluteBoundingBox.y - lo.absoluteBoundingBox.y,
+                };
+              };
+
+              if (child.type === 'ADDR_LONG') {
+                return {
+                  type: 'ADDR_LONG',
+                  fontPostScriptName: child.style.fontPostScriptName,
+                  fontSize: child.style.fontSize,
+                  text: child.characters.split(':')[1],
+                  maxWidth: child.absoluteBoundingBox.width,
+                  lineHeightPx: child.style.lineHeightPx,
+                  x: child.absoluteBoundingBox.x - lo.absoluteBoundingBox.x,
+                  y: child.absoluteBoundingBox.y - lo.absoluteBoundingBox.y,
+                };
+              };
+
+              if (child.type === 'ADDR_COMPACT') {
+                return {
+                  type: 'ADDR_COMPACT',
+                  fontPostScriptName: child.style.fontPostScriptName,
+                  fontSize: child.style.fontSize,
+                  text: child.characters.split(':')[1],
+                  maxWidth: child.absoluteBoundingBox.width,
+                  lineHeightPx: child.style.lineHeightPx,
+                  x: child.absoluteBoundingBox.x - lo.absoluteBoundingBox.x,
+                  y: child.absoluteBoundingBox.y - lo.absoluteBoundingBox.y,
+                };
+              };
+
               console.warn(`Untyped child ${child.name} in flat layouts`)
             }),
           }
         }
       }, {});
 
-      console.log(JSON.stringify(layouts, null, 2));
+      console.log(JSON.stringify(layouts));
     })
   }
 
