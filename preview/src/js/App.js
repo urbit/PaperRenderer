0fbdc0007103dd52ff9fd4ed4e8d93bf04a6f226
Gavin <gvnatknsn@gmail.com>
2019-08-20 16:34:59 -0700

    fix wallet image preview

--- preview/src/js/App.js
+++ preview/src/js/App.js
@@ -7,12 +7,12 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      pages: [],
+      out: [],
     }
   }
 
   handleOutput = (data) => {
-    this.setState({ pages: data })
+    this.setState({ out: data })
     console.log('Out: ', data)
   }
 
@@ -32,14 +32,19 @@ class App extends Component {
             // mode={'REGISTRATION'}
           />
         }
-
-        {this.state.pages.map((p) => {
-          return (
-            <img
-              style={{ maxWidth: '550px', border: '1px solid black' }}
-              src={p.durl}
-            />
-          )
+        {this.state.out.map((wallet) => {
+          return wallet.imagePages.map((page) => {
+            return (
+              <img
+                style={{
+                  maxWidth: '550px',
+                  height: 'auto',
+                  border: '1px solid black',
+                }}
+                src={page.uri}
+              />
+            )
+          })
         })}
       </div>
     )
