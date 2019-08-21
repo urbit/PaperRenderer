0e5d4a148d11667962907e45b6fab9fafa802ba2
francescanarea <francesca@narea.com>
2019-08-21 11:50:45 -0700

    Fix randomized wallet generation

--- tools/genWallet.js
+++ tools/genWallet.js
@@ -11,36 +11,35 @@ const randNumBit = (num) => {
   return Math.random() * (max - min) + min
 }
 
-const main = async () => {
-  const galaxy = {
-    ship: 0,
-    ticket: '~zod',
-  }
-
-  const star = {
-    ship: 9128,
-    ticket: '~binzod',
-  }
-
-  const planet = {
-    ship: 3234674,
-    ticket: '~ridlur',
-  }
-
-  const galaxyWallet = await kg.generateWallet(galaxy)
-  const starWallet = await kg.generateWallet(star)
-  const planetWallet = await kg.generateWallet(planet)
-  console.log(galaxyWallet)
-
-  const wallets = [galaxyWallet, starWallet, planetWallet]
-
-  const json = JSON.stringify(wallets, null, 2)
-
-  fs.writeFileSync(`${OUTPUT_PATH}/sampleWallets.json`, json, (err) => {
-    if (err) throw err
-  })
-}
-
+// const main = async () => {
+//   const galaxy = {
+//     ship: 0,
+//     ticket: '~zod',
+//   }
+//
+//   const star = {
+//     ship: 9128,
+//     ticket: '~binzod',
+//   }
+//
+//   const planet = {
+//     ship: 3234674,
+//     ticket: '~ridlur',
+//   }
+//
+//   const galaxyWallet = await kg.generateWallet(galaxy)
+//   const starWallet = await kg.generateWallet(star)
+//   const planetWallet = await kg.generateWallet(planet)
+//   console.log(galaxyWallet)
+//
+//   const wallets = [galaxyWallet, starWallet, planetWallet]
+//
+//   const json = JSON.stringify(wallets, null, 2)
+//
+//   fs.writeFileSync(`${OUTPUT_PATH}/sampleWallets.json`, json, (err) => {
+//     if (err) throw err
+//   })
+// }
 // main()
 
 const randGalaxy = () => {
@@ -60,62 +59,54 @@ const writeData = (data, path) => {
   })
 }
 
-const createWallet = async () => {
-  const patp = randGalaxy()
-  const num = ob.patp2dec(patp)
-
+const createWallet = async (patp) => {
+  const num = parseInt(ob.patp2dec(patp))
   const config = {
-    ship: 152,
-    ticket: '~fep',
+    ship: num,
+    ticket: patp,
   }
-
-  // config.ship = num
-  // config.ticket = patp
-
-  console.log(config)
-  let wallet = await kg.generateWallet(config)
-  console.log(wallet)
-  // return wallet
+  const wallet = await kg.generateWallet(config)
+  return wallet
 }
-
-createWallet()
 
 // string: outputPath
 // optional bool: planet wallet generation, default true
 // optional bool: star wallet generation, default true
 // optional bool: galaxy wallet generation, default true
 // optional int: num of sampleWallet files to generate, default 1
-// async function createSampleWallets(
-//   outputPath = OUTPUT_PATH,
-//   planet = true,
-//   star = true,
-//   galaxy = true,
-//   num = NUM_WALLET_FILES
-// ) {
-//   // for (var i = 0; i < num; i++) {
-//     var sampleWallet = []
-//     const path = `${outputPath}/sampleWallet.json`
-//
-//     const planetWallet = await createWallet(randPlanet())
-//     const starWallet = await createWallet(randStar())
-//     const galaxyWallet = await createWallet(randGalaxy())
-//
-//     // if (planet) {
-//     //   sampleWallet.push(planetWallet)
-//     // }
-//     // if (star) {
-//     //   sampleWallet.push(starWallet)
-//     // }
-//     // if (galaxy) {
-//     //   sampleWallet.push(galaxyWallet)
-//     // }
-//
-//     writeData(galaxyWallet, path)
-//
-//     console.log(
-//       `Saved ${num} wallet(s) in ${outputPath}. \nWallet types: planet--${planet}, star--${star}, galaxy--${galaxy}`
-//     )
-//   // }
-// }
+const createSampleWallets = async (
+  outputPath = OUTPUT_PATH,
+  planet = true,
+  star = true,
+  galaxy = true,
+  num = NUM_WALLET_FILES
+) => {
+  for (var i = 0; i < num; i++) {
+    var sampleWallet = []
+    const path = `${outputPath}/sampleWallet${i}.json`
 
-// createSampleWallets()
+    const planetWallet = await createWallet(randPlanet())
+    const starWallet = await createWallet(randStar())
+    const galaxyWallet = await createWallet(randGalaxy())
+
+    if (planet) {
+      sampleWallet.push(planetWallet)
+    }
+    if (star) {
+      sampleWallet.push(starWallet)
+    }
+    if (galaxy) {
+      sampleWallet.push(galaxyWallet)
+    }
+
+    writeData(galaxyWallet, path)
+
+    console.log(
+      `Saved ${num} wallet${
+        num > 1 ? 's' : ''
+      } in ${outputPath}. \nWallet types: planet--${planet}, star--${star}, galaxy--${galaxy}`
+    )
+  }
+}
+
+createSampleWallets()
